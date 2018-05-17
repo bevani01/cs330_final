8804cc9d92c8c043bf7fab4b9860a0848ada810a
Nicholas Bevans <bevani01@luther.edu>
2018-05-17 05:57:12 -0700

    Final

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app, db
 from app.forms import LoginForm
 from flask_login import current_user, login_user
